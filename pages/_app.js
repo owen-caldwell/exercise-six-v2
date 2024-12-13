4b2f26a7a4aa2fdf82a0e1157a99f973879a02ce
Owen Caldwell <caldwellom@gmail.com>
2024-12-12 21:33:49 -0500

    dotenv config

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,5 @@
+import dotenv from "dotenv";
+dotenv.config();
 import "@/styles/global.css";
 import { useCallback, useEffect, useState } from "react";
 import { initializeApp } from "firebase/app";
